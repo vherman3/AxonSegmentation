452d81104e388368ec3fd71ef91498fb829a7404
[vherman3] <victor.herman3@gmail.com>
2016-06-02 16:06:37 -0400

    **4

--- groundTruth.py
+++ groundTruth.py
@@ -4,35 +4,37 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import pickle
 
+
 file = 'data/'
 
 img = Image.open(file+'image.png').convert('L')
-img = np.array(img)
+img = np.array(img) # Image associated to the Ground Truth
 h, w = img.shape
 
 im = Image.open(file+'initial_mask.tif').convert('L')
-mask_init = preprocessing.binarize(np.array(im), threshold=100)
+mask_init = preprocessing.binarize(np.array(im), threshold=100) # Initial axon mask built with AxonSegmentation Toolbox
 
 im = Image.open(file+'rectified_mask2.png').convert('L')
-mask_add = preprocessing.binarize(np.array(im), threshold=100)
+mask_add = preprocessing.binarize(np.array(im), threshold=100) # Added true positives in the initial mask
 
 im = Image.open(file+'suppress_mask.tif').convert('L')
-mask_supp = preprocessing.binarize(np.array(im), threshold=100)
+mask_rem = preprocessing.binarize(np.array(im), threshold=100) # Removed False positive in the initial mask
 
-mask = mask_add + (1-mask_supp) * mask_init
+gt = mask_add + (1-mask_rem) * mask_init # GroundTruth
 
 data = {}
 data['image'] = img
-data['mask'] = mask
-
-with open(file+'data2.pkl', 'wb') as handle:
+data['mask'] = gt
+with open(file+'groundTruth.pkl', 'wb') as handle:
     pickle.dump(data, handle)
 
+#######################################################################################################################
+#                                                      Visualization                                                  #
+#######################################################################################################################
 
-fig1 = plt.figure()
-plt.imshow(img, cmap=plt.get_cmap('gray'))
-plt.hold(True)
-plt.imshow(mask, alpha=0.7)
-
-plt.show()
+# fig1 = plt.figure()
+# plt.imshow(img, cmap=plt.get_cmap('gray'))
+# plt.hold(True)
+# plt.imshow(gt, alpha=0.7)
+# plt.show()
 
